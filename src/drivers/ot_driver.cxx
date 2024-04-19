07dfb59230b7196d0836cc022e5902e482066311
Luke West <lukewest73@gmail.com>
2024-04-19 19:12:21 -0400

    debugging printlns

--- src/drivers/ot_driver.cxx
+++ src/drivers/ot_driver.cxx
@@ -47,7 +47,7 @@ void OTDriver::OT_send(std::string m0, std::string m1) {
   auto a = byteblock_to_integer(std::get<1>(a_and_ga));
   auto A = byteblock_to_integer(std::get<2>(a_and_ga));
   SenderToReceiver_OTPublicValue_Message public_value;
-  public_value.public_value = std::get<1>(a_and_ga);
+  public_value.public_value = std::get<2>(a_and_ga);
   network_driver->send(
     crypto_driver->encrypt_and_tag(AES_key, HMAC_key, &public_value)
   );
@@ -61,16 +61,16 @@ void OTDriver::OT_send(std::string m0, std::string m1) {
   receiver_public_value.deserialize(receiver_data_and_ok.first);
 
   auto B = byteblock_to_integer(receiver_public_value.public_value);
-  auto B_to_the_a = CryptoPP::ModularExponentiation(B, a, DL_P);
+  auto B_to_the_a = a_exp_b_mod_c(B, a, DL_P);
   auto inv_A = CryptoPP::EuclideanMultiplicativeInverse(A, DL_P);
   auto B_over_A = a_times_b_mod_c(B, inv_A, DL_P);
-  auto B_over_A_to_the_a = CryptoPP::ModularExponentiation(B_over_A, a, DL_P);
-  std::cout << "before key generation" << std::endl;
+  auto B_over_A_to_the_a = a_exp_b_mod_c(B_over_A, a, DL_P);
+std::cout << "before key generation" << std::endl;
   auto k0 = crypto_driver->AES_generate_key(integer_to_byteblock(B_to_the_a));
   auto k1 = crypto_driver->AES_generate_key(integer_to_byteblock(B_over_A_to_the_a));
-  std::cout << "after key generation" << std::endl;
-  std::cout << B_to_the_a << std::endl;
-  std::cout << B_over_A_to_the_a << std::endl;
+std::cout << "after key generation" << std::endl;
+std::cout << B_to_the_a << std::endl;
+std::cout << B_over_A_to_the_a << std::endl;
   auto c0_and_iv0 = crypto_driver->AES_encrypt(k0, m0);
   auto c1_and_iv1 = crypto_driver->AES_encrypt(k1, m1);
 
@@ -83,7 +83,7 @@ void OTDriver::OT_send(std::string m0, std::string m1) {
     crypto_driver->encrypt_and_tag(AES_key, HMAC_key, &encrypted_values)
   );
 
-  std::cout << "OT_send done" << std::endl;
+std::cout << "OT_send done" << std::endl;
 }
 
 /*
@@ -109,8 +109,8 @@ std::string OTDriver::OT_recv(int choice_bit) {
   auto b_and_gb = crypto_driver->DH_initialize();
   auto b = byteblock_to_integer(std::get<1>(b_and_gb));
   auto gb = byteblock_to_integer(std::get<2>(b_and_gb));
-  std::cout << gb << std::endl;
-  std::cout << CryptoPP::ModularExponentiation(DL_G, b, DL_P) << std::endl;
+std::cout << gb << std::endl;
+std::cout << a_exp_b_mod_c(DL_G, b, DL_P) << std::endl;
   CryptoPP::Integer pub = (choice_bit == 0) ? gb : a_times_b_mod_c(gb, A, DL_P);
 
   ReceiverToSender_OTPublicValue_Message public_value;
@@ -129,20 +129,20 @@ std::string OTDriver::OT_recv(int choice_bit) {
   ciphertexts.deserialize(ciphertexts_data_and_ok.first);
 
 
-  std::cout << "before key generation in recv" << std::endl;
-  std::cout << CryptoPP::ModularExponentiation(A, b, DL_P) << std::endl;
+std::cout << "before key generation in recv" << std::endl;
+std::cout << a_exp_b_mod_c(A, b, DL_P) << std::endl;
   auto k = crypto_driver->AES_generate_key(
     integer_to_byteblock(
-      CryptoPP::ModularExponentiation(A, b, DL_P)
+      a_exp_b_mod_c(A, b, DL_P)
     )
   );
-  std::cout << "before key generation in recv" << std::endl;
+std::cout << "before key generation in recv" << std::endl;
 
   if (choice_bit) {
-    std::cout << "decrypting e1" << std::endl;
+std::cout << "decrypting e1" << std::endl;
     return crypto_driver->AES_decrypt(k, ciphertexts.iv1, ciphertexts.e1);
   } else {
-    std::cout << "decrypting e0" << std::endl;
+std::cout << "decrypting e0" << std::endl;
     return crypto_driver->AES_decrypt(k, ciphertexts.iv0, ciphertexts.e0);
   }
 }
